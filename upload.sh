4e959c07190e2d7f6b6b413d511b0e117008752c
lichuanjun <lichuanjun@niwodai.net>
2018-01-18 13:56:26 +0800

    修改upload.sh

--- upload.sh
+++ upload.sh
@@ -1,21 +1,22 @@
 #!/bin/bash
 
-git stash
-git pull origin master --tags
-git stash pop
+# git stash
+# git pull origin master --tags
+# git stash pop
 
-VersionString=`grep -E 's.version.*=' TLAModuleSwift.podspec`
-VersionNumber=`tr -cd 0-9 <<<"$VersionString"`
+# VersionString=`grep -E 's.version.*=' TLAModuleSwift.podspec`
+# VersionNumber=`tr -cd 0-9 <<<"$VersionString"`
 
-NewVersionNumber=$(($VersionNumber + 1))
-LineNumber=`grep -nE 's.version.*=' TLAModuleSwift.podspec | cut -d : -f1`
-sed -i "" "${LineNumber}s/${VersionNumber}/${NewVersionNumber}/g" TLAModuleSwift.podspec
+# NewVersionNumber=$(($VersionNumber + 1))
+# LineNumber=`grep -nE 's.version.*=' TLAModuleSwift.podspec | cut -d : -f1`
+# sed -i "" "${LineNumber}s/${VersionNumber}/${NewVersionNumber}/g" TLAModuleSwift.podspec
 
-echo "current version is ${VersionNumber}, new version is ${NewVersionNumber}"
+# echo "current version is ${VersionNumber}, new version is ${NewVersionNumber}"
 
-git add .
-git commit -am ${NewVersionNumber}
-git tag ${NewVersionNumber}
-git push origin master --tags
-cd ~/.cocoapods/repos/TLPods && git pull origin master && cd - && pod repo push TLPods TLAModuleSwift.podspec --verbose --allow-warnings --use-libraries
+# git add .
+# git commit -am ${NewVersionNumber}
+# git tag ${NewVersionNumber}
+# git push origin master --tags
+# cd ~/.cocoapods/repos/TLPods && git pull origin master && cd - && 
+pod repo push TLPods TLAModuleSwift.podspec --verbose --allow-warnings
 
